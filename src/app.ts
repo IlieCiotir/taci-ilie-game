5cc951442e09ca59bf1d5231a49e02e91de5833c
angular-cli <angular-cli@angular.io>
2015-11-30 14:36:03 +0200

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {TaciIlieApp} from './app/taci-ilie';
+
+
+bootstrap(TaciIlieApp);
---
+++ src/app/taci-ilie.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'taci-ilie-app',
+  providers: [],
+  templateUrl: 'app/taci-ilie.html',
+  directives: [],
+  pipes: []
+})
+export class TaciIlieApp {
+
+  constructor() {}
+
+}
